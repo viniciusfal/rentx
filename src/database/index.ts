acb29615e48791b7838d6cca3a3197e7652fddf5
Vinicius <vinicius_prsilva@hotmail.com>
2023-01-06 01:42:37 -0300

    chore: Typeorm configured

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,5 @@
 import { DataSource } from 'typeorm';
+import { CreateCategories1672976133007 } from './migrations/1672976133007-CreateCategories';
 
 const datasource = new DataSource({
   type: 'postgres',
@@ -6,6 +7,9 @@ const datasource = new DataSource({
   username: 'docker',
   password: 'ignite',
   database: 'rentx',
+  migrations: [CreateCategories1672976133007],
 });
 
 datasource.initialize();
+
+export default datasource;
